12af9fd14e61c28077cb3e13d25462cbc7d6b6f7
Anuj Tyagi <tyagi.an@husky.neu.edu>
2020-05-09 18:26:34 -0400

    Update 3_dinosaur.py

--- fbe/file_parsing/3_dinosaur.py
+++ fbe/file_parsing/3_dinosaur.py
@@ -6,10 +6,10 @@ def printBipedalDinosaursOrderBySpeed(filePathDinoInfo, filePathAddInfo):
     g = 9.8
     with open(filePathAddInfo, 'r') as f:
         line = csv.reader(f)
-        print (line)
+        #print (line)
         while line:
             line = f.readline().strip()
-            print (line)
+            #print (line)
             if line:
                 NAME, STRIDE_LENGTH, STANCE = line.split(',')
                 if STANCE == "bipedal":
@@ -25,9 +25,8 @@ def printBipedalDinosaursOrderBySpeed(filePathDinoInfo, filePathAddInfo):
                     STRIDE_LENGTH, LEG_LENGTH = bipedalDinosaurs[NAME], float(LEG_LENGTH)
                     bipedalDinosaurs[NAME] = ((STRIDE_LENGTH / LEG_LENGTH) - 1) * math.sqrt(LEG_LENGTH * g)
     
-    print (bipedalDinosaurs)
-    #heap = [(value, key) for key, value in bipedalDinosaurs.items()]
-    #fastest = heapq.nlargest(len(heap), heap)  # O(n + mlogn)
-    #print(*[name for speed, name in fastest], sep='\n')
+    #print ([key for key, value in sorted(bipedalDinosaurs.items(), key = lambda kv: kv[1], reverse=True) ])
+    for key, value in sorted(bipedalDinosaurs.items(), key = lambda kv: kv[1], reverse=True):
+        print("%s: %s" %(key, value))
 
 printBipedalDinosaursOrderBySpeed('dataset_1.csv', 'dataset_2.csv')
