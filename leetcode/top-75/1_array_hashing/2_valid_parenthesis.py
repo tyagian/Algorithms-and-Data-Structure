e6d27ac7de7e646527c59d8be3f85f73967b9132
tyagian <tyagi.an@northeastern.edu>
2022-05-21 11:43:45 -0400

    valid parenthesis

---
+++ leetcode/top-75/1_array_hashing/2_valid_parenthesis.py
@@ -0,0 +1,25 @@
+#https://leetcode.com/problems/valid-parentheses/
+# This can be solved by stack also but using hash-map here.
+class test:
+    def valid_parenthesis(s):
+        brackets_dict = {'(':')' , '[':']' , '{':'}'}
+        open_brackets = []
+        
+        for bracket in s:
+            if bracket in brackets_dict:
+                open_brackets.append(bracket)
+            else:
+                # if open_brack list not empty and open_brackets[-1] is value of key in brackets_dict 
+                if len(open_brackets) and brackets_dict[open_brackets[-1]] == bracket:
+                    del open_brackets[-1]
+                else:
+                    return False
+        return open_brackets == []
+
+def main():
+    input_ = "{()[]}"
+    output = test.valid_parenthesis(input_)
+    print (output)
+
+if __name__ == '__main__':
+    main()
