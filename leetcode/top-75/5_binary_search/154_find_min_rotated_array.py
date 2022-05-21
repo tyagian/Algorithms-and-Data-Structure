bda06f133df9c02bcdf1a1b6e05d1b46943a6986
tyagian <tyagi.an@northeastern.edu>
2022-05-21 19:11:24 -0400

    min in rotated sorted array

--- leetcode/top-75/5_binary_search/154_find_min_rotated_array.py
+++ leetcode/top-75/5_binary_search/154_find_min_rotated_array.py
@@ -6,9 +6,26 @@ memory: O(log n) since binary search used
 """
 class test:
 
-    def find_min():
+    def find_min(arr):
+        low, high = 0, len(arr) - 1
 
+        if arr[low] <= arr[high]:
+            return arr[low]
 
-nums = [4,5,6,7,0,1,2]
-t = test.find_min(nums)
+        while low <= high:
+            if low == high:
+                return low
+            mid = (low+high) // 2
+            print (f"low, high, mid: {low}, {high}, {mid} and {arr[mid]}")
+            if mid < high and arr[mid+1] < arr[mid]:
+                return arr[mid+1]
+            if mid > low and arr[mid] < arr[mid-1]:
+                return arr[mid]
+            if arr[mid] < arr[high]:
+                high = mid-1
+            else:
+                low = mid + 1
+ 
+arr = [4,5,6,7,-1,0,1,2]
+t = test.find_min(arr)
 print (t)
